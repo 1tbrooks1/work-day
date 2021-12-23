81609551c7083de157afd2b8b0438e47eb470e79
tbrooks <tbrooks321@yahoo.com>
2021-12-23 08:47:31 -0500

    created function to start on load

--- Develop/script.js
+++ Develop/script.js
@@ -1,2 +1,19 @@
+$(document).ready(function(){
+    // listen for button clicks
+    $(".save-btn").on("click", function(){
+       
+        // get value of <textarea> and assign it to variable
+        var task = $(this).siblings(".text").val();
+        console.log(task);
+        // get value of time in timeblock and assign to variable .parent
+        var time = $(this).parent().attr("id");
+        console.log(time);
+
+        // save value into localStorage.setItem
+    })
+    // immediate function to check for localStorage
+
+    // a selector for each time block
+})
 
 
